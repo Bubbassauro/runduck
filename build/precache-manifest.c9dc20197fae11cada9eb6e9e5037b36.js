8b5a2f13c84ef23dc519b83e108513558200cdfc
Tatiana Hanazaki <tati.hana@equinox.com>
2020-04-13 08:38:03 -0400

    Include build without preset paths

---
+++ build/precache-manifest.c9dc20197fae11cada9eb6e9e5037b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382c99fe2990356e11b921d55b350cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06962818b7a41f079f99",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d527703de036d80624b1",
+    "url": "/static/js/2.ad9eb523.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.ad9eb523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06962818b7a41f079f99",
+    "url": "/static/js/main.e1613fa1.chunk.js"
+  },
+  {
+    "revision": "c6666d2c53ff3230cb66",
+    "url": "/static/js/runtime-main.b01f62ef.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3825/precache-manifest.b83d3667c76b8ba43911f3b319cb82fe.js"
+  "/precache-manifest.c9dc20197fae11cada9eb6e9e5037b36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3825/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
